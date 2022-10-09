6697d2a3ff4be238f473564dac1bf4ba6324bf8a
Bobby <simplyhappydev@gmail.com>
2022-10-09 14:53:56 -0500

    Attempting to access out of bounds array element

---
+++ reading_documentation.1/out-of-bounds.js
@@ -0,0 +1,11 @@
+/*
+What happens if we take the array ['fish', 'and', 'chips'] and try to access the element at index position 10? Try this in your JavaScript console.
+*/
+
+['fish', 'and', 'chips'][10]; // returns the primitive value => undefined
+
+console.log(['fish', 'and', 'chips'][10]);
+
+/**LS
+ * Attempting to access an element at an index that is out of the bounds of the array will return undefined.
+ */
