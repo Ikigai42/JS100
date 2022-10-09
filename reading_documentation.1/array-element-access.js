c8f5efe4a50abb9044333abb80f6c1be3e65a102
Bobby <simplyhappydev@gmail.com>
2022-10-09 14:51:06 -0500

    Array element access with bracket notation

---
+++ reading_documentation.1/array-element-access.js
@@ -0,0 +1,17 @@
+/*
+Locate the documentation for the Array built-in object on MDN.
+
+How can we access the element 'and' in the array ['fish', 'and', 'chips']?
+*/
+
+let elementAccess = ['fish', 'and', 'chips'];
+
+elementAccess[1]; //Use bracket notation and the index value
+
+/*
+We can use bracket notation to access the array element at a particular index. In JavaScript, arrays have a zero-based index. This means that the first element in the array will have an index of 0, the second element will have an index of 1, and so on.
+*/
+['fish', 'and', 'chips'][1];
+
+let dinner = ['fish', 'and', 'chips'];
+dinner[1];
