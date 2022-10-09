ee5a549d36e2b1e9084c4c7f11f0eba0bc180a15
Bobby <simplyhappydev@gmail.com>
2022-10-09 14:32:24 -0500

    String.prototype.toLowerCase() method

---
+++ reading_documentation.1/lowercase.js
@@ -0,0 +1,5 @@
+// Find out whether JavaScript has a method to lowercase a string, for example converting 'Aloha, World!' into 'aloha, world!.
+
+String.prototype.toLowerCase(); //converts all the alphabetic characters in a string to lowercase
+
+String.prototype.toLocaleLowerCase(); //takes into account the host's current locale
