c601770090f53adf001083ecbf90a78761efd9a1
Bobby <simplyhappydev@gmail.com>
2022-10-10 01:59:22 -0500

    Unix epoch jan 1 1970

---
+++ reading_documentation.2/5.date-object.js
@@ -0,0 +1,17 @@
+// Find out what Date.now() returns.
+
+/* per MDN:
+The static Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+*/
+
+/**LS
+ * Discussion
+This way of representing a point in time is called Unix time or epoch milliseconds. Chances are high that you will encounter and use Unix time quite a few times throughout your future career. Check out this Wired article if you want to read a bit more about it.
+
+Further Exploration
+The Date documentation on MDN provides a lot of good information about Date objects and how to work with them. Take some time to explore this information and familiarize yourself with the methods available to you.
+
+https://www.wired.com/2001/09/unix-tick-tocks-to-a-billion/
+ */
