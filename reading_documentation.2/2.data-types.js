0566044727cfa0ed6476cb8ba858fac34cc58437
Bobby <simplyhappydev@gmail.com>
2022-10-10 00:27:16 -0500

    Listing JS primitive data types

---
+++ reading_documentation.2/2.data-types.js
@@ -0,0 +1,26 @@
+// Without looking into your notes, try to remember which primitive data types are defined in JavaScript. Then find this information on MDN.
+
+/** 7 Primitive Data Types
+ String
+ Number
+ Boolean
+ Null
+ Undefined
+ (newer 👇)
+ BigInt
+ Symbol
+ *
+ 
+ 
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types#data_types
+
+Seven data types that are primitives:
+- Boolean | true and false.
+- null | special keyword denoting a null value. (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
+- undefined | A top-level property whose value is not defined.
+- Number | An integer or floating point number. For example: 42 or 3.14159.
+- BigInt | An integer with arbitrary precision. For example: 9007199254740992n.
+- String | A sequence of characters that represent a text value. For example: "Howdy".
+- Symbol | A data type whose instances are unique and immutable.
+
+ */
