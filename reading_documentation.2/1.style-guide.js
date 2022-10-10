22acfd524678b02b535e0483dde19968c120824e
Bobby <simplyhappydev@gmail.com>
2022-10-09 19:52:36 -0500

    Added LS solution

--- reading_documentation.2/1.style-guide.js
+++ reading_documentation.2/1.style-guide.js
@@ -22,3 +22,15 @@ while (iceCreamDensity > 0) {
 }
 
 console.log('The ' + iceCreamTaste + ' ice cream melted.');
+
+/**LS
+ *Discussion
+Here are the guidelines that were not followed in the original code snippet:
+
+Use camelCase for variable names.
+End all statements with a semicolon.
+Indent lines with two spaces.
+Use spaces between keywords (like while) and opening parentheses, as well as between closing parentheses and opening curly brackets.
+Put the opening curly brackets of the while block on the same line as the while statement.
+Put spaces before and after operators like +. 
+*/
