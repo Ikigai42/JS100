07af3a74f8393142bfaaf9b8441d33dd60914b0e
Bobby <simplyhappydev@gmail.com>
2022-10-12 14:26:20 -0500

    Implicit return value of function: undefined

---
+++ functions1/2.log-quote.js
@@ -0,0 +1,17 @@
+/**Write a function that logs Brendan Eich's quote 'Always bet on JavaScript.' to the console. What is the return value of the function? */
+
+let quote = 'Always bet on JavaScript.';
+
+function brendanEichQuote(quote) {
+  console.log(quote); // Return Value => undefined
+}
+
+brendanEichQuote(quote);
+// logs:
+// Always bet on JavaScript.
+
+// Return Value => undefined (that's the implicit return value when there isn't a value returned)
+
+/**LS
+ * Since the function has no return statement, the return value of our function is undefined.
+ */
