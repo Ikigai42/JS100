eb5b175bbf5afa8d9f36f501910ae48d670e0481
Bobby <simplyhappydev@gmail.com>
2022-10-13 13:35:05 -0500

    .toUpperCase() method

---
+++ strings/2.all-caps.js
@@ -0,0 +1,12 @@
+// Take the string 'confetti floating everywhere' and transform it to upper case.
+
+String.prototype.toUpperCase();
+
+console.log('confetti floating everywhere'.toUpperCase());
+
+// CONFETTI FLOATING EVERYWHERE
+
+/**LS
+ * Discussion
+Our solution leverages the String.prototype.toUpperCase() method. This method returns a new string, with the calling string's characters converted to uppercase. To see that this method returns a new string rather than mutating the original one, log string to the console after line 3.
+ */
