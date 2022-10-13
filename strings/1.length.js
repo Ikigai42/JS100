129651c3a554995bf786643cea4805da7dd1aed2
Bobby <simplyhappydev@gmail.com>
2022-10-13 13:25:57 -0500

    String .length property

---
+++ strings/1.length.js
@@ -0,0 +1,9 @@
+// Determine the length of the string "These aren't the droids you're looking for.".
+
+console.log("These aren't the droids you're looking for.".length);
+// 43
+
+/**LS
+ * Discussion
+We can determine the length of a string using the length property. An empty string will have a length of 0.
+ */
