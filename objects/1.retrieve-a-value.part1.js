ba77a1d3f48c76243cc4253ddaade28d2c83504d
Bobby <simplyhappydev@gmail.com>
2022-10-14 17:31:47 -0500

    Accessing object's property with dot/bracket notation

---
+++ objects/1.retrieve-a-value.part1.js
@@ -0,0 +1,22 @@
+// Write the code necessary to retrieve the value of the courses property of our student object.
+
+let student = {
+  name: 'Carmen',
+  age: 14,
+  grade: 10,
+  courses: ['biology', 'algebra', 'composition', 'ceramics'],
+  gpa: 3.75,
+};
+
+student.courses;
+
+console.log(student.courses); // [ 'biology', 'algebra', 'composition', 'ceramics' ]
+console.log(student.lunch); // undefined
+
+/**LS
+ * Discussion
+We can access the property value using either dot notation or bracket notation. JavaScript style guides typically recommend using dot notation when possible.
+
+Further Exploration
+What is returned if you attempt to access an object property that does not exist? Try it out for yourself, for example calling student.locker with the above example object. Especially notice that no exception is raised.
+ */
